9177884fbddb61332094a52587ea3e95397c8587
Izaak Salek <izaaksalek@gmail.com>
2020-03-25 19:38:28 -0300

    Adicionadas as interfaces DepartmentDao e SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.Department;
+
+public interface DepartmentDao {
+	
+	public void insert(Department obj);
+	public void update(Department obj);
+	public void deleteById(Integer id);
+	public Department findById(Integer id);
+	public List<Department> findAll();
+	
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.Seller;
+
+public interface SellerDao {
+	
+	public void insert(Seller obj);
+	public void update(Seller obj);
+	public void deleteById(Integer id);
+	public Seller findById(Integer id);
+	public List<Seller> findAll();
+	
+}
